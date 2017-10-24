afe29b758cd1d6765a7396c2d491308baaa8e1fd
Jon Tran <jtran30@uic.edu>
2017-10-24 03:00:00 -0500

    Proj 2 refine

---
+++ cs491VR/js/myjsPS2.js
@@ -0,0 +1,314 @@
+//DATK8, MUSE SIAB, CODE PRO, Capture it, Montserrant, EXO2
+
+
+
+///////////////
+////Below are for video control
+///////////////
+
+var toggleVideo = false;
+// var myVideo = document.getElementById("vid1");
+
+
+function playOrPause() {
+    /*
+    if (myVideo.paused) {
+        myVideo.play();
+        //console.log("Playing")
+    }
+    else {
+        myVideo.pause();
+        //console.log("Pause")
+    }
+    */
+}
+
+function stopVideo() {
+    //myVideo.pause();
+}
+
+/*
+$("#vid1").bind("ended", function() {
+    $("#postVideoMsg").fadeIn();
+});
+*/
+
+function replayVideo() {
+    $("#postVideoMsg").fadeOut();
+    myVideo.play();
+}
+
+
+
+
+///////////////
+////Above are for video control
+///////////////
+
+
+
+
+
+$("#entirePageSize").hide();
+$(".popOutContentPadding").hide();
+
+
+var allowScrolling = false;
+$(document).ready(function() {
+
+    if (!/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) { //Test if user is using mobile
+        $("#parallaxPictures").css("display", "block");
+    }
+    else {
+
+    }
+
+
+    $(window).scrollTop(0);
+
+    $("#mainBody").css("height", 0);
+
+    $("#loadingBar").addClass("animateLoadingBar");
+
+allowScrolling = true; //Allows scrolling when the video is now playing. (Load is finished)
+        resetHeight();
+   //After 2 seconds of "fake loading", go do the animation
+
+
+
+
+    $(document).click(function() { //Debugger
+        //$("#loadingScreen").hide();
+        //resetHeight();
+        //allowScrolling = true;
+    });
+});
+
+
+
+function getHeight() {
+    var height = 0; //This runs once, and adjusts the height of the page based off on the div's in it.
+    $(".frontWrapper > div").each(function() {
+        height += $(this).height();
+    });
+    height += $(window).innerHeight();
+
+    return height;
+}
+
+function resetHeight() {
+    var height = 0; //This runs once, and adjusts the height of the page based off on the div's in it.
+    $(".frontWrapper > div").each(function() {
+        height += $(this).height();
+    });
+    height += $(window).innerHeight() * 2;
+
+    $("#mainBody").css("height", height);
+
+
+    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) { //Test if user is using mobile
+        $("#parallaxPictures").css("display", "none");
+    }
+    else {
+        $("#parallaxPictures").css("display", "block"); //Display parallax or not
+    }
+
+}
+
+window.addEventListener('resize', function() { //Resizing may affect the page's height so we need to recalc the height 
+    resetHeight();
+});
+
+
+
+
+
+
+var curHeight = 0;
+$(window).scroll(function(event) { //Allows smoother scrolling
+    if (allowScrolling == false) {
+        return;
+    }
+
+
+    var scroll = $(window).scrollTop() * -1;
+
+    //console.log(scroll);
+    $(".frontWrapper").css("transform", "translate3d(0," + scroll + "px,0)");
+    $(".fronterWrapper").css("transform", "translate3d(0," + scroll / 2 + "px,0)");
+    $(".back1Wrapper").css("transform", "translate3d(0," + scroll + "px,0)");
+    $(".back3Wrapper").css("transform", "translate3d(0," + scroll / 4 + "px,0)");
+    // Do something
+});
+
+
+
+function slideTo(num) {
+
+    if (num == 1) {
+        $(window).scrollTop($(window).height());
+        var scroll = $(window).scrollTop() * -1;
+        $(".frontWrapper").css("transform", "translate3d(0," + scroll + "px,0)");
+        $(".fronterWrapper").css("transform", "translate3d(0," + scroll / 2 + "px,0)");
+        $(".back1Wrapper").css("transform", "translate3d(0," + scroll + "px,0)");
+        $(".back3Wrapper").css("transform", "translate3d(0," + scroll / 4 + "px,0)");
+    }
+
+}
+
+
+var openedPopup = 0; //keeps track of the current popup state
+var selectedSlide = -1; // was about, shop, or story clicked?
+
+function openPopup(num) {
+
+    selectedSlide = num;
+
+    if (openedPopup == 0 && num == 1) {
+
+        $("#aboutPopout").addClass("animatePopup");
+        $("#aboutPopout .popOutContentPadding").fadeIn(900);
+       
+        $(window).bind('mousewheel', function(e) { // on scroll
+            var popupDiv = $('#aboutPopout');
+            // set div scroll top offset to current + extra from this scroll
+            popupDiv.scrollTop(popupDiv.scrollTop() -
+                e.originalEvent.wheelDelta);
+
+            return false; // prevent body scrolling
+        });
+    }
+
+
+    if (openedPopup == 0 && num == 2) {
+
+
+
+
+        $("#shopPopout").addClass("animatePopup");
+        $("#shopPopout .popOutContentPadding").fadeIn(900);
+        
+        $(window).bind('mousewheel', function(e) { // on scroll
+            var popupDiv = $('#shopPopout');
+            // set div scroll top offset to current + extra from this scroll
+            popupDiv.scrollTop(popupDiv.scrollTop() -
+                e.originalEvent.wheelDelta);
+
+            return false; // prevent body scrolling
+        });
+    }
+
+
+
+    if (openedPopup == 0 && num == 3) {
+
+        $("#storyPopout").addClass("animatePopup");
+        $("#storyPopout .popOutContentPadding").fadeIn(900);
+        
+        $(window).bind('mousewheel', function(e) { // on scroll
+            var popupDiv = $('#storyPopout');
+            // set div scroll top offset to current + extra from this scroll
+            popupDiv.scrollTop(popupDiv.scrollTop() -
+                e.originalEvent.wheelDelta);
+
+            return false; // prevent body scrolling
+        });
+    }
+
+    if (openedPopup == 0 && num == 4) {
+
+        $("#shopPopout").addClass("animatePopup");
+        $("#shopPopout .popOutContentPadding").fadeIn(900);
+        $(window).bind('mousewheel', function(e) { // on scroll
+            var popupDiv = $('#shopPopout');
+            // set div scroll top offset to current + extra from this scroll
+            popupDiv.scrollTop(popupDiv.scrollTop() -
+                e.originalEvent.wheelDelta);
+
+            return false; // prevent body scrolling
+        });
+    }
+
+
+}
+
+
+function closePopup() {
+
+
+    if (openedPopup == 1) {
+        openedPopup = 2;
+
+
+        $(".popOut").removeClass("animatePopup");
+        $(".popOutContentPadding").fadeOut(600);
+        if (selectedSlide == 1) {
+            $("#aboutPopout").addClass("animateClosePopup");
+        }
+        if (selectedSlide == 2) {
+            $("#shopPopout").addClass("animateClosePopup");
+        }
+        if (selectedSlide == 3) {
+            $("#storyPopout").addClass("animateClosePopup");
+        }
+        if (selectedSlide == 4) {
+            $("#eventPopout").addClass("animateClosePopup");
+        }
+    }
+
+}
+
+$(".popOut").bind('oanimationend animationend webkitAnimationEnd', function() {
+    if (openedPopup == 0) {
+        openedPopup = 1;
+    }
+    if (openedPopup == 2) {
+        openedPopup = 0;
+        $(".popOut").removeClass("animateClosePopup");
+        $(window).unbind("mousewheel");
+    }
+});
+
+
+
+function switchTab(num) {
+
+    $(".tab1, .tab2").addClass("hideItem");
+
+    if (num == 1) {
+        $(".tab1").removeClass("hideItem");
+    }
+    if (num == 2) {
+        $(".tab2").removeClass("hideItem");
+    }
+
+}
+
+
+
+function seeHwProj(num) {
+    if (num == 1) {
+        $("#homeworks").fadeOut();
+        $("#projects").fadeIn();
+    }
+    else {
+        $("#projects").fadeOut();
+        $("#homeworks").fadeIn();
+    }
+}
+
+
+///////////////
+////Contact Menu Button
+///////////////
+
+function openContactMenu() {
+    $("#entirePageSize").fadeIn();
+}
+
+$("#entirePageSize").click(function(e) { //Handle click events
+    $("#entirePageSize").hide();
+})
+$("#entirePageSize #contactMenu").click(function(e) { //Prevents SCROLLING in child subclasses
+    e.stopPropagation();
+});
